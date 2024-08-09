d37d208603357034deb054ac4f6465f7b126adc1
Bevilhonda <marcelinyuri@gmail.com>
2024-08-09 11:18:37 -0300

    atualiza classe DTO do update

--- src/main/java/com/farmacia/ApiFarmacia/Controller/Funcionario/Atualizar/DTO/RequisicaoDeDados.java
+++ src/main/java/com/farmacia/ApiFarmacia/Controller/Funcionario/Atualizar/DTO/RequisicaoDeDados.java
@@ -2,15 +2,19 @@ package com.farmacia.ApiFarmacia.Controller.Funcionario.Atualizar.DTO;
 
 import com.farmacia.ApiFarmacia.Model.Funcionario.FuncionarioEntity;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 
 public class RequisicaoDeDados {
     @JsonProperty("id_funcionario")
     private Integer id_funcionario;
     @JsonProperty("nome")
+    @NotBlank(message = "Digite o campo nome.")
     private String nome;
     @JsonProperty("sobrenome")
+    @NotBlank(message = "Digite o campo sobrenome.")
     private String sobrenome;
     @JsonProperty("cargo")
+    @NotBlank(message = "Digite o campo cargo.")
     private String cargo;
 
     public RequisicaoDeDados(Integer id_funcionario, String nome, String sobrenome, String cargo) {
